6832f16ce73a636cc21eb9566c41659f02398119
joepollastrini <joe.pollastrini@gmail.com>
2020-08-11 16:11:56 -0500

    Add files via upload

---
+++ Automation/applicant db update.py
@@ -0,0 +1,4 @@
+### This sript will grab responses from the online form (Google Forms)
+### It will then update the locally saved database
+### It will return any new applicants since the last update, as a new database
+
